4804ebb07a3184970ae6d32a1233ae7d6e574f67
AngelRionCervi <a.rioncervi@gmail.com>
2024-05-29 13:40:14 +0200

    done template

--- src/routes/article/[id]/+page.svelte
+++ src/routes/article/[id]/+page.svelte
@@ -2,14 +2,26 @@
   export let data;
 
   const { id, title, body } = data.article;
+
+  let isSaved = false;
 </script>
 
 <div class="article-page">
+  <a class="favorite-link" href="/favoris">mes favoris</a>
   <div class="article-content">
-    <div class="article-section"><span>id:</span>{id}</div>
+    <div class="article-head">
+      <div class="article-section"><span>id:</span>{id}</div>
+      <button class="save-article-button">
+        {#if isSaved}
+          <img src="/src/assets/heart-filled.svg" width="40" alt="article is saved" />
+        {:else}
+          <img src="/src/assets/heart-empty.svg" width="40" alt="article is note saved" />
+        {/if}
+      </button>
+    </div>
     <div class="article-section title">{title}</div>
     <div class="delimiter" />
-    <div class="article-section body">{body}{body}{body}{body}{body}</div>
+    <div class="article-section body">{body}{body}{body}{body}</div>
   </div>
 </div>
 
@@ -19,6 +31,12 @@
     height: 100vh;
     display: flex;
     justify-content: center;
+  }
+
+  .article-head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
   }
 
   .article-content {
@@ -36,8 +54,8 @@
     gap: 8px;
 
     &.title {
-        font-size: 36px;
-        font-weight: bold;
+      font-size: 36px;
+      font-weight: bold;
     }
   }
 
@@ -46,5 +64,20 @@
     width: 30%;
     margin: 6px 0;
     background-color: whitesmoke;
+  }
+
+  .save-article-button {
+    background-color: transparent;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+  }
+
+  .favorite-link {
+    font-size: 20px;
+    position: absolute;
+    top: 16px;
+    right: 16px;
+    color: whitesmoke;
   }
 </style>
